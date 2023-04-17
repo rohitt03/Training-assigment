59473740a4a79e34d81fa1dfc93c923b579398a2
rohitt03 <rohitpatil12199@gmail.com>
2023-04-17 17:34:03 +0530

    calculator2

--- Assigment 1/Calculator/Calculator/Program.cs
+++ Assigment 1/Calculator/Calculator/Program.cs
@@ -8,65 +8,171 @@ namespace Calculator
 {
     interface ICalculator
     {
-        int sum(int a, int b);
-        int sum(params int[] a);
-        int sub(int a, int b);
-        int mul(int a, int b);
-        int mul(params int[] a);
-        int div(int a, int b);
+        int sum(int num1, int num2);
+        int sum(params int[] num);
+        int sub(int num1, int num2);
+        int sub(int[] num);
+        int mul(int num1, int num2);
+        int mul(params int[] num);
+        int div(int num1, int num2);
+        int div(params int[] num);
+
 
     }
-    class Program:ICalculator
+    class Program : ICalculator
     {
         static void Main(string[] args)
         {
             Program p = new Program();
-            Console.WriteLine(p.sum(1, 2));
-            Console.WriteLine(p.sum(1, 2, 3));
-            Console.WriteLine(p.sub(5,4));
-            Console.WriteLine(p.mul(2, 3));
-            Console.WriteLine(p.mul(2, 3, 4, 5));
-            Console.WriteLine(p.div(6, 3));
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Choose the opertaion \n 1 addition \n 2 substraction \n 3 multiplication \n 4 division \n 5 Exit");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter size ");
+                int size = int.Parse(Console.ReadLine());
+                int num1=0, num2=0;
+                int [] num = new int[size];
+                if (size == 2)
+                {
+                    Console.WriteLine("Enter two variable");
+                    num1 = int.Parse(Console.ReadLine());
+                    num2= int.Parse(Console.ReadLine());
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Enter " + size + " number");
+                    for(int i = 0; i < size; i++)
+                    {
+                        num[i]= int.Parse(Console.ReadLine());
+                    }
+                }
+                if (size<= 1) {
+                    Console.WriteLine("Please enter valid size");
+                    choice=5;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        if (size == 2)
+                        {
+                            Console.WriteLine(p.sum(num1, num2));
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine(p.sum(num));
+                        }
+
+                        break;
+
+                    case 2:
+                        if (size == 2)
+                        {
+                            Console.WriteLine(p.sub(num1, num2));
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(p.sub(num));
+                        }
+                        break;
+
+                    case 3:
+                        if (size == 2)
+                        {
+                            Console.WriteLine(p.mul(num1, num2));
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(p.mul(num));
+                        }
+                        break;
+
+                    case 4:
+                        if (size == 2)
+                        {
+                            Console.WriteLine(p.div(num1, num2));
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(p.div(num));
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+
+                }
+
+            } while (choice != 5);
+            Console.WriteLine("---------------Thank you------------------");
             Console.ReadKey();
 
+
         }
 
-        public int div(int a, int b)
+        public int div(int num1, int num2)
         {
-            return a / b;
+            return num1 / num2;
         }
 
-        public int mul(int a, int b)
+        public int div(params int[] num)
         {
-            return a * b;
-        }
-
-        public int mul(params int[] a)
-        {
-            int temp=1;
-            for(int i = 0; i < a.Length; i++)
+            int temp = num[0];
+            for (int i = 1; i < num.Length; i++)
             {
-                temp = temp * a[i];
+                temp = temp / num[i];
             }
             return temp;
         }
 
-        public int sub(int a, int b)
+        public int mul(int num1, int num2)
         {
-            return a - b;
+            return num1 * num2;
         }
 
-        public int sum(int a, int b)
+        public int mul(params int[] num)
         {
-            return a + b;
-        }
-
-        public int sum(params int[] a)
-        {
-            int temp=0;
-            for(int i = 0; i < a.Length; i++)
+            int temp = 1;
+            for (int i = 0; i < num.Length; i++)
             {
-                temp = temp + a[i];
+                temp = temp * num[i];
+            }
+            return temp;
+        }
+
+        public int sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int sub(int[] num)
+        {
+            int temp=num[0];
+            for(int i = 1; i < num.Length; i++)
+            {
+                temp = temp - num[i];
+            }
+            return temp;
+        }
+
+        public int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int sum(params int[] num)
+        {
+            int temp = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                temp = temp + num[i];
             }
             return temp;
         }
